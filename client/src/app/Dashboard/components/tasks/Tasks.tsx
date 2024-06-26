6c31ff7f92efd61308928d655e35e12bdff0f09f
AdelRyad <adelryad51@gmail16.com>
2024-06-26 06:08:49 +0300

    first commit

--- client/src/app/Dashboard/components/tasks/Tasks.tsx
+++ client/src/app/Dashboard/components/tasks/Tasks.tsx
@@ -1,51 +1,33 @@
-// pages/tasks.js
-
+import React from "react";
+import Task from "./Task";
+import DropDown from "../DropDown";
 import axios from "axios";
-import Task from "../components/Task";
-import DropDown from "../components/DropDown";
-import CreateTask from "../components/CreateTask";
-
-export async function getServerSideProps() {
-  const res = await axios.get(process.env.NEXT_PUBLIC_API + "/api/tasks", {
-    headers: {
-      "Cache-Control": "no-cache",
-    },
-  });
+import { task } from "@/app/Dashboard/components/tasks/Task";
+const Tasks = async () => {
+  const res = await axios.get(process.env.NEXT_PUBLIC_API + "/api/tasks");
   const data = res.data;
-  return {
-    props: { tasks: data }, // will be passed to the page component as props
-  };
-}
-
-const TasksPage = ({ tasks }: { tasks: any }) => {
-  const handleTaskCreated = () => {
-    // Re-fetch data or update state to force re-rendering with new data
-  };
-
+  console.log(data);
   return (
-    <div>
-      <div className="flex flex-col w-full h-full gap-4">
-        <div className="flex justify-between w-full items-center h-fit">
-          <h1 className="text-2xl font-semibold">Tasks</h1>
-          <DropDown title="Today" options={["Last Week", "Last Month"]} />
-        </div>
-        <div className="overflow-auto scroll-bar">
-          {tasks.map((task) => (
-            <Task
-              key={task._id}
-              title={task.title}
-              date={task.date}
-              time={task.time}
-              status={task.status}
-              description={task.description}
-              attachment={task.attachment}
-            />
-          ))}
-        </div>
+    <div className="flex flex-col w-full h-full gap-4 ">
+      <div className="flex justify-between w-full items-center h-fit ">
+        <h1 className="text-2xl font-semibold">Tasks</h1>
+        <DropDown title="Today" options={["Last Week", "Last Month"]} />
       </div>
-      <CreateTask onTaskCreated={handleTaskCreated} />
+      <div className="overflow-auto scroll-bar">
+        {data.map((task: task) => (
+          <Task
+            key={task._id}
+            title={task.title}
+            date={task.date}
+            time={task.time}
+            status={task.status}
+            description={task.description}
+            attachment={task.attachment}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default TasksPage;
+export default Tasks;
