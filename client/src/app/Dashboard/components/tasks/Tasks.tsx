7472a601e423413fd3eb291a622e820171eb163a
AdelRyad <adelryad51@gmail16.com>
2024-06-26 05:46:34 +0300

    first commit

--- client/src/app/Dashboard/components/tasks/Tasks.tsx
+++ client/src/app/Dashboard/components/tasks/Tasks.tsx
@@ -4,7 +4,11 @@ import DropDown from "../DropDown";
 import axios from "axios";
 import { task } from "@/app/Dashboard/components/tasks/Task";
 const Tasks = async () => {
-  const res = await axios.get(process.env.NEXT_PUBLIC_API + "/api/tasks");
+  const res = await axios.get(process.env.NEXT_PUBLIC_API + "/api/tasks", {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
   const data = res.data;
   return (
     <div className="flex flex-col w-full h-full gap-4 ">
